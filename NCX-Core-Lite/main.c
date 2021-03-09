d17eb9503f08242edc3ea396994612907adc5f3b
Ian Skinner <56081713+IanSkinner1982@users.noreply.github.com>
2021-03-09 17:55:45 -0500

    Updated stuff

--- NCX-Core-Lite/main.c
+++ NCX-Core-Lite/main.c
@@ -17,33 +17,6 @@ int Download();
 // Declare variables
 int menuChoice;
 // Start code
-void Update(){
-    #ifdef _WIN32
-    #endif
-    #ifdef unix
-        clrScrn();
-        printf("Downloading...");
-        if(!Download("https://github.com/NCX-Programming/NCX-Core-Lite/releases/latest/download/NCX-Core-Lite-linux.zip", "NCX-Core-Lite-linux.zip")){printf("error\n");};
-        clrScrn();
-        printf("Self-updating is still a WIP. A zip file containing the latest release has been downloaded, however you will have to extract it yourself.\n\n");
-        printf("Press ENTER to return to the store.\n");
-        getchar();
-        getchar();
-        menuChoice=0;
-        Store();
-    #endif
-    #ifdef __APPLE__
-        clrScrn();
-        printf("Downloading...");
-        if(!Download("https://github.com/NCX-Programming/NCX-Core-Lite/releases/latest/download/NCX-Core-Lite-X86.zip", "NCX-Core-Lite-X86.zip")){printf("error\n");};
-        clrScrn();
-        printf("Self-updating is still a WIP. A zip file containing the latest release has been downloaded, however you will have to extract it yourself.\n\n");
-        printf("Press ENTER to return to the store.\n");
-        getchar();
-        menuChoice=0;
-        Store();
-    #endif
-}
 void Store(){
     clrScrn();
     printf("*====================================*\n");
@@ -51,23 +24,23 @@ void Store(){
     printf("*=======*============================*\n");
     printf("|       |                            |\n");
     printf("|  (1)  |   Exit Program             |\n");
-    printf("|       |                            |\n");
-    printf("|  (2)  |   Update NCX-Core-Lite     |\n");
-    printf("|       |                            |\n");
+	printf("|       |                            |\n");
     printf("|       | Programs:                  |\n");
     printf("|       |                            |\n");
-    printf("|  (3)  |   theVaultC                |\n");
+    printf("|  (2)  |   C64-Title-Loader         |\n");
     printf("|       |                            |\n");
-    printf("|  (4)  |   fakeApt                  |\n");
+    printf("|  (3)  |   Cookie-Clicker-C64       |\n");
     printf("|       |                            |\n");
-    printf("|  (5)  |   fake-pacman              |\n");
+    printf("|  (4)  |   Tic-Tac-Toe-C64          |\n");
     printf("|       |                            |\n");
-    printf("|  (6)  |   lazy-dsi-file-downloader |\n");
+    printf("|  (5)  |   Math-Game-C64            |\n");
     printf("|       |                            |\n");
-    printf("|  (7)  |   C64-Title-Loader         |\n");
-    printf("|       |                            |\n");
-    printf("|  (8)  |   Empty Slot               |\n");
-    printf("|       |                            |\n");
+    //printf("|  (6)  |                            |\n");
+    //printf("|       |                            |\n");
+    //printf("|  (7)  |                            |\n");
+    //printf("|       |                            |\n");
+    //printf("|  (8)  |                            |\n");
+    //printf("|       |                            |\n");
     printf("*=======*============================*\n");
     menuChoice=0;
     while(menuChoice==0||menuChoice=='\n'){
@@ -77,38 +50,17 @@ void Store(){
         if(menuChoice==49){
             clrScrn();
             exit(0);}
-        if(menuChoice==50)Update();
-        if(menuChoice==51)vaultc();
-        if(menuChoice==52)fkapt();
-        if(menuChoice==53)fkpcmn();
-        if(menuChoice==54)ldsifd();
-        if(menuChoice==55)c64tl();
-        if(menuChoice==56)Store();}
+        if(menuChoice==50)TL64();
+        if(menuChoice==51)CC64();
+        if(menuChoice==52)TTT64();
+        if(menuChoice==53)MG64();
+        //if(menuChoice==54)();
+        //if(menuChoice==55)();
+        //if(menuChoice==56)();}
 }
 int main(void){
-    printf("Loading...\n");
-    DIR* dir = opendir("tmp");
-    if (dir) {
-        // Directory already exists, just continue
-        closedir(dir);
-    }else if (ENOENT == errno) {
-        // Directory doesn't exist, so make it
-        mkdir("tmp/",0777); }
-    if(!Download("https://raw.githubusercontent.com/NinjaCheetah/NCX-Installer-news/master/newsLatest.txt", "tmp/news.txt")){printf("error\n");}
-    clrScrn();
-    printf("Welcome to NCX-Core-Lite!\n\n");
+    printf("Welcome to Ian's fork of NCX-Core-Lite!\n\n");
     printf("NCX-Core-Lite,  Copyright (C) 2021  NCX-Programming/NinjaCheetah\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; view the full license in the repo.\n\n");
-    // Read news file
-    FILE *fp;
-    char news[2048];
-    fp=fopen("tmp/news.txt","r");
-    if (!fp)
-        return 1;
-    while (fgets(news,2048, fp)!=NULL)
-    printf("NCX-News: %s\n\n", news);
-    // conclude the news reading session (news is for old people anyway)
-    fclose(fp);
-    // Prompt to continue to the store (currently you have no choice- *you must shop*)
     printf("Press ENTER to view available software");
     getchar();
     Store();
--- NCX-Core-Lite/store.c
+++ NCX-Core-Lite/store.c
@@ -6,108 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 // Declare variables
-int storeChoice;     
-void ldsifd(){
-    clrScrn();
-    printf("lazy-dsi-file-downloader\nBy YourKalamity\n\n");
-    printf("Automatically downloads, extracts and places files used for DSi modding.\n\n");
-    printf("Requirements: At least Python 3.5 with the Tkinter library\n\n");
-    printf("Options:\n1. Download\n2. Return to Store\n");
-    storeChoice=0;
-    while(storeChoice==0||storeChoice=='\n'){
-        storeChoice=0;
-        storeChoice=fgetc(stdin);
-        // Get choice
-        if(storeChoice==49){
-            #ifdef unix
-                clrScrn();
-                printf("Downloading...\n");
-                if(!Download("https://github.com/YourKalamity/lazy-dsi-file-downloader/releases/download/3.1.2/lazy-dsi-file-downloader-3.1.2-Python3.zip", "lazy-dsi-file-downloader-3.1.2-Python3.zip")){printf("Done.\n");};
-                sleep(1);
-                ldsifd();
-            #endif
-            #ifdef __APPLE__
-                clrScrn();
-                printf("Downloading...\n");
-                if(!Download("https://github.com/YourKalamity/lazy-dsi-file-downloader/releases/download/3.1.2/lazy-dsi-file-downloader-3.1.2-macOS.zip", "lazy-dsi-file-downloader-3.1.2-macOS.zip")){printf("Done.\n");};
-                sleep(1);
-                ldsifd();
-            #endif 
-            }
-        if(storeChoice==50)Store();}
-}
-void vaultc(){
-    clrScrn();
-    printf("theVaultC\nBy NCX-Programming/NinjaCheetah\n\n");
-    printf("An updated and upgraded version of theVault, now in C. Available for Windows, Linux, and Intel Macs. \n\n");
-    printf("Requirements: macOS: 10.12 or later, Linux: 64-bit version of Linux, Windows: 32 or 64-bit version of Windows\n\n");
-    printf("Options:\n1. Download\n2. Return to Store\n");
-    storeChoice=0;
-    while(storeChoice==0||storeChoice=='\n'){
-        storeChoice=0;
-        storeChoice=fgetc(stdin);
-        // Get choice
-        if(storeChoice==49){
-            clrScrn();
-            printf("Downloading...\n");
-            if(!Download("https://github.com/NCX-Programming/theVaultC/releases/latest/download/theVault-ALL.zip", "theVault-ALL.zip")){printf("Done.\n");};
-            sleep(1);
-            vaultc(); }
-        if(storeChoice==50)Store();}
-}
-void fkapt(){
-    clrScrn();
-    printf("fakeApt\nBy NinjaCheetah\n\n");
-    printf("A fun little program made in C# with .NET 5, just goes through a fake apt install process to 'install' any package you could think up.\n\n");
-    printf("Requirements: .NET 5.0 on macOS, Windows, or Linux\n\n");
-    printf("Options:\n1. Download\n2. Return to Store\n");
-    storeChoice=0;
-    while(storeChoice==0||storeChoice=='\n'){
-        storeChoice=0;
-        storeChoice=fgetc(stdin);
-        // Get choice
-        if(storeChoice==49){
-            clrScrn();
-            printf("Downloading...\n");
-            if(!Download("https://github.com/NinjaCheetah/fake-apt/releases/latest/download/fakeApt-latest-stable.zip", "fakeApt-latest-stable.zip")){printf("Done.\n");};
-            sleep(1);
-            fkapt(); }
-        if(storeChoice==50)Store();}
-}
-void fkpcmn(){
-    clrScrn();
-    printf("fake-pacman\nBy Jonatan6\n\n");
-    printf("fake-pacman is a terminal program for macOS and Linux that will make it look like you are installing a pacman package.\n\n");
-    printf("Requirements: macOS: 10.12 or later, Linux: 64-bit version of Linux\n\n");
-    printf("Options:\n1. Download\n2. Return to Store\n");
-    storeChoice=0;
-    while(storeChoice==0||storeChoice=='\n'){
-        storeChoice=0;
-        storeChoice=fgetc(stdin);
-        // Get choice
-        if(storeChoice==49){
-            #ifdef unix
-                clrScrn();
-                printf("Downloading...\n");
-                if(!Download("https://github.com/Jonatan6/fake-pacman/releases/latest/download/fake-pacman-linux.zip", "fake-pacman-linux.zip")){printf("Done.\n");};
-                sleep(1);
-                fkpcmn();
-            #endif
-            #ifdef __APPLE__
-                clrScrn();
-                printf("Downloading...\n");
-                if(!Download("https://github.com/Jonatan6/fake-pacman/releases/latest/download/fake-pacman-mac.zip", "fake-pacman-mac.zip")){printf("Done.\n");};
-                sleep(1);
-                fkpcmn();
-            #endif 
-            }
-        if(storeChoice==50)Store();}
-}
-void c64tl(){
+int storeChoice;
+void TL64(){
     clrScrn();
     printf("C64-Title-Loader\nBy IanSkinner1982\n\n");
     printf("A program to load all of my C64 projects.\n\n");
-    printf("Requirements: VICE Emulator on macOS, Windows, or Linux\n\n");
+    printf("Requirements: VICE Emulator on macOS, Windows, or Linux (or 3DS lol)\n\n");
     printf("Options:\n1. Download\n2. Return to Store\n");
     storeChoice=0;
     while(storeChoice==0||storeChoice=='\n'){
@@ -118,6 +22,63 @@ void c64tl(){
             clrScrn();
             printf("Downloading...\n");
             if(!Download("https://github.com/IanSkinner1982/C64-title-loader/releases/latest/download/loader.d64", "loader.d64")){printf("Done.\n");};
+            sleep(1);
+            c64tl(); }
+        if(storeChoice==50)Store();}
+}
+void CC64(){
+    clrScrn();
+    printf("Cookie-Clicker-C64\nBy IanSkinner1982\n\n");
+    printf("A WIP cookie clicker clone for the Commodore 64.\n\n");
+    printf("Requirements: VICE Emulator on macOS, Windows, or Linux (or 3DS lol)\n\n");
+    printf("Options:\n1. Download\n2. Return to Store\n");
+    storeChoice=0;
+    while(storeChoice==0||storeChoice=='\n'){
+        storeChoice=0;
+        storeChoice=fgetc(stdin);
+        // Get choice
+        if(storeChoice==49){
+            clrScrn();
+            printf("Downloading...\n");
+            if(!Download("https://github.com/IanSkinner1982/Cookie-Clicker-C64/releases/latest/download/Cookie.Clicker.d64", "cookie.d64")){printf("Done.\n");};
+            sleep(1);
+            c64tl(); }
+        if(storeChoice==50)Store();}
+}
+void TTT64(){
+    clrScrn();
+    printf("Tic-Tac-Toe-C64\nBy IanSkinner1982\n\n");
+    printf("Tic Tac Toe for the Commdore 64.\n\n");
+    printf("Requirements: VICE Emulator on macOS, Windows, or Linux (or 3DS lol)\n\n");
+    printf("Options:\n1. Download\n2. Return to Store\n");
+    storeChoice=0;
+    while(storeChoice==0||storeChoice=='\n'){
+        storeChoice=0;
+        storeChoice=fgetc(stdin);
+        // Get choice
+        if(storeChoice==49){
+            clrScrn();
+            printf("Downloading...\n");
+            if(!Download("https://github.com/IanSkinner1982/Tic-Tac-Toe-C64/releases/latest/download/tictactoe.d64", "tic-tac-toe.d64")){printf("Done.\n");};
+            sleep(1);
+            c64tl(); }
+        if(storeChoice==50)Store();}
+}
+void MG64(){
+    clrScrn();
+    printf("C64-Title-Loader\nBy IanSkinner1982\n\n");
+    printf("A program to load all of my C64 projects.\n\n");
+    printf("Requirements: VICE Emulator on macOS, Windows, or Linux (or 3DS lol)\n\n");
+    printf("Options:\n1. Download\n2. Return to Store\n");
+    storeChoice=0;
+    while(storeChoice==0||storeChoice=='\n'){
+        storeChoice=0;
+        storeChoice=fgetc(stdin);
+        // Get choice
+        if(storeChoice==49){
+            clrScrn();
+            printf("Downloading...\n");
+            if(!Download("https://github.com/IanSkinner1982/Random-BASIC-Projects/blob/main/Math-Game-C64/build/math.prg", "math-game.prg")){printf("Done.\n");};
             sleep(1);
             c64tl(); }
         if(storeChoice==50)Store();}
